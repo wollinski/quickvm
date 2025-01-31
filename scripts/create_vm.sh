39e86bff6a3894120dd52176182f8502933c2ba5
wolle <wolle@wollinski.net>
2025-01-31 15:18:57 +0100

    add script that creates a VM around an image file located in `.isolation/ubuntu.img` relative to workdir also it applies cloud init placed in `.isolation/cloudinit`
    the VM name in libvirt contains the workdir: `isolationvm_$(basename "$WORKDIR")`

---
+++ scripts/create_vm.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+WORKDIR="$(pwd)"
+
+# ToDo: Create config dir dynamically with some init method
+CONFIGDIR=".isolation"
+
+# ToDo: This should not be in every CONFIGDIR but in a central place or downloaded
+DISKIMAGE="${CONFIGDIR}/ubuntu.img"
+CLOUDINIT_DATA="${CONFIGDIR}/cloudinit"
+
+# Create name dynamically so that it contains directory where script is invoked
+VMNAME="isolationvm_$(basename "$WORKDIR")"
+
+virt-install \
+    --name "$VMNAME" \
+    --memory 4096 \
+    --vcpus 4 \
+    --os-variant ubuntu22.04 \
+    --nographics \
+    --disk path="$DISKIMAGE" \
+    --import \
+    --cloud-init user-data="${CLOUDINIT_DATA}/user-data",meta-data="${CLOUDINIT_DATA}/meta-data"
